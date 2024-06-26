2dbe868609692da521a6271235587a7775fe0a2d
Prince Jaiswal <princejaiswal278@gmail.com>
2024-06-26 19:16:03 +0530

    more on constructor

--- constructor.js
+++ constructor.js
@@ -1,7 +1,10 @@
 class Triangle {
   constructor(a, b) {
-    //the this keyword inside the constructor refers to the object instantiated 
+    //the this keyword inside the constructor refers to the object instantiated
     //from this Triangle class and not the class itself
+    // A constructor is automatically called whenever we create a new instance of the class.
+    //Two most common things a constructor is used for is to validate data that is passed to the constructor and sign properties.
+    //constructor functions always returns “undefined”. So we should never try to return anything from the constructor.
     this.a = a;
     this.b = b;
   }
